535ddbf1e94f8d25e5f7cbc69b4d5946a327631e
zxiaoy <zxy1850759271@163.com>
2019-08-19 22:13:08 +0800

    主页面清理

--- src/App.js
+++ src/App.js
@@ -13,50 +13,23 @@ class App extends React.Component {
     super(props);
   }
 
-  addOne = () => {
-    this.props.JGAction.changeValue();
-  }
 
   render() {
-    console.log('zxy', JGActions);
     return  (
       <div>
-        <p>react App homepage <span>{this.props.JGStore.value}</span></p>
-        <Button type='primary' onClick={this.addOne}>加1</Button>
+        <p>react App homepage</p>
       </div>
     );  
   }
 }
 
-const mapStateToProps = state => ({
-    JGStore: state.JGReducer
-});
+// const mapStateToProps = state => ({
+//     JGStore: state.JGReducer
+// });
 
-const mapDispatchToProps = dispatch => ({
-  JGAction: bindActionCreators(JGActions, dispatch)
-});
+// const mapDispatchToProps = dispatch => ({
+//   JGAction: bindActionCreators(JGActions, dispatch)
+// });
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-
-
-
-
-
-// // 测试devServer.contentBase
-// console.log('zxy1');
-// let titleElement = document.createElement('p');
-// titleElement.innerText = "js内部代码"
-// document.body.appendChild(titleElement);
-
-// // 测试babel/core es6语法解析
-// let fn = () => {
-//   console.log('何不臧');
-// };
-// fn();
-
-// // 测试babel-runtime， es6 api解析
-// function * gen() {
-//   yield 1
-// } 
-// console.log(gen().next());
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+export App;
